4a545026db120c576ddcc4c8fdbc8c7b00bf872a
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-02 21:15:52 -0500

    place-controllers.js created

--- models/http-error.js
+++ models/http-error.js
@@ -1,8 +1,8 @@
-class httpError extends Error{
+class HttpError extends Error{
     constructor(message, errorCode ){
    super(message) // add a message property
    this.code = errorCode // adds a code property
     }
 }
 
-module.exports = httpError
+module.exports = HttpError
