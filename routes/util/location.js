5e50c08afb7a6d0c2428b46424dffa3eecb1e512
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-08 19:16:00 -0500

    geolocation set google api key

--- routes/util/location.js
+++ routes/util/location.js
@@ -1,3 +1,20 @@
 const axios =require('axios')
+const HttpError = require('../../models/http-error')
+
+
 
 const API_KEY= 'AIzaSyATzBBWEsecVs7Oa4BiTGLIkaScnOhrDYs'
+
+async function getCoordsForAddress(address){
+  const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${API_KEY}`
+  )
+  const data = response.data
+  if( !data || data.status === 'ZERO_RESULTS'){
+  const error = new HttpError('Could not find a location for the specified address.',
+   422 
+   )
+   throw error
+  }
+  const coordinates = data.results[0].geomatry.location;
+  return coordinates;
+}
