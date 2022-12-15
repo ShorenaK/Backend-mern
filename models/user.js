a327dc5d10d7bd2f59e59756fba80d05e46d1341
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-15 13:25:28 -0500

    user model schema created

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: { type: String , require: true }, 
+    email: { type: String , require: true, unique: true}, 
+    password: { type: String , require: true , minlength: 4}, 
+    image: { type: String , require: true }, 
+    places: { type: String , require: true }
+})
+
+
+module.exports = mongoose.model('User', userSchema) 
