2afbdb24aea56d7d7a822c56226053f7108665a5
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-14 21:16:23 -0500

    bug is fixed CRUD started

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -21,19 +21,28 @@ let DUMMY_PLACES = [
     creator: 'u1'
   }
 ];
-
-const getPlaceById = (req, res, next) => {
+const getPlaceById = async(req, res, next) => {
   const placeId = req.params.pid; // { pid: 'p1' }
 
-  const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId;
-  });
+  let place
+ try { 
+  const place =  await Place.findById(placeId)
+} catch(err){
+ const error = new HttpError(
+   'Something went wring, could not find a place.', 500
+ );
+ return next (error)
+}
 
   if (!place) {
-    throw new HttpError('Could not find a place for the provided id.', 404);
+   const error =  new HttpError(
+    'Could not find a place for the provided id.', 
+    404
+    )
+    return next(error)
   }
-
-  res.json({ place }); // => { place } => { place: place }
+  
+  res.json({ place: place.toObject({ getters: true }) }); // => { place } => { place: place }
 };
 
 // function getPlaceById() { ... }
@@ -49,7 +58,7 @@ const getPlacesByUserId = (req, res, next) => {
   if (!places || places.length === 0) {
     return next(
       new HttpError('Could not find places for the provided user id.', 404)
-    );
+    )
   }
 
   res.json({ places });
