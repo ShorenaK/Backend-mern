e1197f49b074f0193043795d796d2e60f15dd29f
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-17 22:04:45 -0500

    cleaned files from comment

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); // => /api/places...
+app.use('/api/places', placesRoutes); 
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
@@ -54,46 +54,4 @@ mongoose.connection.on('disconnected', () => console.log('MongoDB disconnected  
 
 
 
-
-
-
-
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const mongoose = require('mongoose')
-// require('dotenv').config()
-// const connectionStr = process.env.MONGODB_URI
-
-// const placesRoutes = require('./routes/places-routes');
-// const usersRoutes = require('./routes/users-routes');
-// const HttpError = require('./models/http-error');
-
-
-// const app = express();
-
-// app.use(bodyParser.json());
-
-// app.use('/api/places', placesRoutes); 
-// app.use('/api/users', usersRoutes);
-
-// app.use((req, res, next) => {
-//   const error = new HttpError('Could not find this route.', 404);
-//   throw error;
-// });
-
-// app.use((error, req, res, next) => {
-//   if (res.headerSent) {
-//     return next(error);
-//   }
-//   res.status(error.code || 500)
-//   res.json({message: error.message || 'An unknown error occurred!'});
-// });
-
-// mongoose.connect(connectionStr)
-// .then(()=>{
-//   app.listen(7000);
-// })
-// .catch(err =>{
-//   console.log(err)
-// })
 
