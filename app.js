925253bd01b341ac38695a414da04bb77080e1f2
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-21 18:19:15 -0500

    app middleware for pic upload added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 const express = require('express');
 const app = express();
@@ -17,6 +18,10 @@ const place = require('./models/place');
 // app.use(express.json())
 
 app.use(bodyParser.json());
+
+
+app.use('/uploads/images', 
+express.static(path.join('uploads', 'images')))
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
