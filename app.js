77e9594acbd673dc8a7c1e56e4b28bb88acf2c24
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-21 18:04:07 -0500

    files updated

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -37,13 +39,17 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, err => {
+      console.log(err);
+    });
+  }
   if (res.headerSent) {
     return next(error);
   }
   res.status(error.code || 500);
   res.json({ message: error.message || 'An unknown error occurred!' });
 });
-
 
 mongoose
   .connect(connectionStr)
