a6d12c3cfed975fa91eba13d627b3e960539da63
ShorenaK <shorenaanzhilov@gmail.com>
2022-12-22 17:32:46 -0500

    hashed added

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -47,12 +47,24 @@ const signup = async (req, res, next) => {
     );
     return next(error);
   }
+let hashedPassword; 
+// hash and salt == 12
+try{
+ hashedPassword  = await bcrypt.hash(password, 12)
+
+}catch(err){
+  const error = new HttpError(
+    "Could not create user, please try again.", 
+    500
+  )
+  return next(error)
+}
 
   const createdUser = new User({
     name,
     email,
     image: req.file.path,
-    password,
+    password: hashedPassword,
     places: [],
   });
 
